7dfc74db9d2407565104d5b93541b1e74b7092b9
Rep! <anto.9623.ar@gmail.com>
2021-08-31 21:16:55 +0200

    I metodi sono stati separati

--- src/Scraping_Criptovalute.java
+++ src/Scraping_Criptovalute.java
@@ -21,57 +21,41 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class Scraping_Criptovalute {
-
-
-	public static void main( String[] args) throws InterruptedException, IOException
-	{
-
-		String link = "https://criptovalute.io/"; 
-
-
-
-		WebDriver driver;
-		Scanner scan = new Scanner(System.in);
-		
+	
+	public static WebDriver MakeChromedriver(){
 		URL path1 = Scraping_Criptovalute.class.getResource("chromedriver.exe");
 		File file = new File(path1.getFile());
-		
-		//File file = new File("chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		return driver;
+	}	
+	
+	public static String NameForCsv (){
+		Date oggi = new Date();
+		String name="Criptovalute_"+oggi.getDate()+oggi.getMonth()+oggi.getYear()+"_"+oggi.getHours()+oggi.getMinutes()+".csv";
+		return name;
+	}
+	
+	public static void main( String[] args) throws InterruptedException, IOException{
+		String link = "https://criptovalute.io/"; 
+		WebDriver driver = MakeChromedriver();
 		driver.manage().window().setSize(new Dimension(900,700)); //ALLARGO FINESTRA PER SELEZIONE NAV
-		
-		Calendar calendar = new GregorianCalendar();
-		String day=calendar.get(calendar.DAY_OF_MONTH)+"-"+calendar.get(calendar.MONTH);
-		String path="crypto_"+day+".csv";
-
-		BufferedWriter writer = new BufferedWriter(new FileWriter(path));
-
-		Vector<String> today_in_tv = new Vector<String>();
-
-
+		String csvfile=NameForCsv();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(csvfile));
 		try {
 			driver.get(link);
 			Thread.sleep(3000);
-
 			String nome = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div/div[2]/div[2]/div[1]/div[1]/b/a")).getText();
 			String prezzo = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div/div[2]/div[2]/div[1]/div[2]")).getText();
-
 			Vector<String> cripto = new Vector<String>();
-			
 			String nomi_col="Nome,Prezzo USD, Cambio 24H \n";
 			writer.write(nomi_col);
-			
 			for (int i=2; i<102; i++) {
-				
 				String nome_c = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div/div["+i+"]/div[2]/div[1]/div[1]/b/a")).getText();
 				String prezzo_c = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div/div["+i+"]/div[2]/div[1]/div[2]")).getText();
 				String cambio_c = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div/div["+i+"]/div[2]/div[1]/div[3]/div")).getText();
-				
 				String crip= nome_c + ","+ prezzo_c +","+cambio_c+"\n";
-				
-				writer.write(nome_c + ","+ prezzo_c +","+cambio_c+"\n");
-				
+				writer.write(nome_c + ","+ prezzo_c +","+cambio_c+"\n");	
 				cripto.add(crip);
 			}
 			writer.close();
@@ -81,9 +65,7 @@ public class Scraping_Criptovalute {
 			e.printStackTrace();
 		} finally {
 			driver.close();
-
 		}
-
 	}
 
 
